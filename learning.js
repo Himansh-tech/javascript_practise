accbc6f6a883d634201b5c23ad8344194222bfeb
Himanshu Pandey <90492268+Himansh-tech@users.noreply.github.com>
2023-08-18 00:05:59 +0530

    Update learning.js

--- learning.js
+++ learning.js
@@ -12,7 +12,7 @@ let c=add(1,2,54);
 console.log(c);
 ________________________________________
 
-CODE 2:Programm which  takes age as input and tell if you can drive or not using Ternary operator
+//CODE 2:Programm which  takes age as input and tell if you can drive or not using Ternary operator
 let age=prompt("Age boliye?")
 let dedo=Number.parseInt(age)
 console.log((dedo>18)? "You can drive":" Bhag yaha se")
@@ -22,7 +22,7 @@ for(let i=0;i<=5;++i){
     console.log(i)
 }
 
-CODE 4:  programm to exchange/SWAP values two variable a and b
+//CODE 4:  programm to exchange/SWAP values two variable a and b
 
 var temp="0";
 var a="3";
@@ -34,12 +34,12 @@ b=temp                // and atlast "b" me "a" ki value aani chaiye, aur wo stor
 console.log("a is: "+a)
 console.log("b is: "+b)
 
-CODE 5: We can concatenate strings using “+” operator.
+//CODE 5: We can concatenate strings using “+” operator.
 var a="Hello"
 var b=" Everyone"
 alert(a+b)
 
-CODE 6: code to, only allow 140 character to be entered by user. like in twitter we can only write 140 charactes long 	Tweet.
+//CODE 6: code to, only allow 140 character to be entered by user. like in twitter we can only write 140 charactes long 	Tweet.
 let max=140;
 let tweet_text=prompt("Please enter your tweet....");
 
@@ -50,18 +50,18 @@ console.log("only:: "+ tweet + " ::can be tweeted");
 // console.log(leng.length);
 // leng=leng.slice(0,140)
 // console.log(leng);
-CODE 7: Code to take input a name and then return “Hello Name” where first leteer of name is capital doesn’t matter how input is given.
+//CODE 7: Code to take input a name and then return “Hello Name” where first leteer of name is capital doesn’t matter how input is given.
 let name=prompt("Give your name: ");
 alert("Hello "+name.slice(0,1).toUpperCase()+name.slice(1,name.length).toLowerCase());
 /*various things is done in above code I have merged everything as one. i.e first of all we take out first letter of name, make it capital and also we make other letter of name small and then concatenate them and print */
 
 
-CODE 8: equivalent human age for given dogage
+//CODE 8: equivalent human age for given dogage
 let  dogage=prompt("Enter age of dog");
 let humanage=((dogage-2)*4)+21;
 alert("Human's age equivalent to that Dog's age is "+ humanage)
 
-CODE 9: Prints age left in Days, weeks and months assume our final age is 90
+//CODE 9: Prints age left in Days, weeks and months assume our final age is 90
 function lifeInWeeks(age) {
     var daysLeft; // x 365
     var weeksLeft; // y 52
@@ -73,13 +73,13 @@ function lifeInWeeks(age) {
 }
 lifeInWeeks(89);
 
-CODE  10:  prints the love percentage randomly. Using “math.random”
+//CODE  10:  prints the love percentage randomly. Using “math.random”
 let name1=prompt("Enter boy's name");
 let name2=prompt("Enter girl's name");
 let num=Math.random();
 alert("love percentage is: "+Math.round(num*100));
 
-CODE 11:function to print number from 1 to 100 but, we print “Fizz” if number is divisible by 3 and “Buzz” if number id divisible by 5 and “FizzBuzz” if it is divisible by both 3 and 5.
+//CODE 11:function to print number from 1 to 100 but, we print “Fizz” if number is divisible by 3 and “Buzz” if number id divisible by 5 and “FizzBuzz” if it is divisible by both 3 and 5.
  var output=[];
 let num=1;
 function fizzbuzz(){
@@ -101,7 +101,7 @@ function fizzbuzz(){
     console.log(output);
  }
  fizzbuzz();
-CODE 12: function to print fibonnaci series till given number
+//CODE 12: function to print fibonnaci series till given number
 function fibonnaci(n){
 var arr=[];
 if(n===0){
@@ -126,7 +126,7 @@ else{
 console.log(arr);
 }
 fibonnaci(5)
- CODE 13: switch case to tell about your age.
+// CODE 13: switch case to tell about your age.
 let age = prompt("What is your age?");
 switch (true) {
   case age < 10:
@@ -139,7 +139,7 @@ switch (true) {
     console.log("Age is greater than 20");
     break;
 }
-CODE 14: code using Switch case to check if it is divisible by 3 or 5;
+//CODE 14: code using Switch case to check if it is divisible by 3 or 5;
 let num=prompt("Give the num to check it's divisibilty with 2 or 3")
 num=Number.parseInt(num)
 let by2=num%2
@@ -152,15 +152,15 @@ switch(by2==0 || by3==0){
         console.log("no")
 }
 
- CODE 15: JS function in newest syntax to print sum of two numbers
+ //CODE 15: JS function in newest syntax to print sum of two numbers
 let sum = (a, b) => a+b;
 sum(2,3)
- CODE 16: Another syntax for functions (here we have to use console.log to print)
+// CODE 16: Another syntax for functions (here we have to use console.log to print)
 let printf=(a,b) =>{
     console.log(a+b)
 };
 printf(2,3);
-CODE 17: old syntax where we have to use “function” keyword to use functions
+//CODE 17: old syntax where we have to use “function” keyword to use functions
 function sum(a,b) {
     console.log(a+b) //instead of console.log we can use “return”
 };
@@ -187,7 +187,7 @@ console.log(marks[Object.keys(marks)[i]])
 }
 
 
-Code 19: same as above question but here we are using for..in loop to access each key and then print their respective marks.
+//Code 19: same as above question but here we are using for..in loop to access each key and then print their respective marks.
 for(let key in marks){
     console.log("The marks of "+ key + " are "+ marks[key])
 }
@@ -199,7 +199,7 @@ console.log(sentence)
 after variable “sentence” it is not comma but it  is backtick and we put the variables inside ${variable}
 
 
-CODE 20: Escape sequence
+//CODE 20: Escape sequence
 let fruit='ban\'ana'
 console.log(fruit)
 console.log(fruit.length)
